9bfb63947bfbf2b0326ac43c196e40170ac987ed
johnny hannayounan <johnny.hannayounan@isae.edu.lb>
2022-02-13 11:24:54 +0200

    tp5

--- tp5/question1/Ensemble.java
+++ tp5/question1/Ensemble.java
@@ -4,44 +4,52 @@ import java.util.*;
 
 public class Ensemble<T> extends AbstractSet<T> {
 
-	protected java.util.Vector<T> table = new java.util.Vector<T>();
+    protected java.util.Vector<T> table = new java.util.Vector<T>();
 
-	public int size() {
-		return table.size();
-	}
+    public int size() {
+        return table.size();
+    }
 
-	public Iterator<T> iterator() {
-		return table.iterator();
-	}
+    public Iterator<T> iterator() {
+        return table.iterator();
+    }
 
-	public boolean add(T t) {
-		// à compléter pour la question1-1
+    public boolean add(T t) {
+        if(table.contains(t)){return false;}
+        else{table.add(t);return true;}
+        
+    }
 
-		return false;
-	}
+    public Ensemble<T> union(Ensemble<? extends T> e) {
+        this.addAll(e); 
+        return this;
+    }
 
-	public Ensemble<T> union(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
+    public Ensemble<T> inter(Ensemble<? extends T> e) {
+            if(e.contains(table)){
+            Ensemble<T> e1=new Ensemble<T>();
+            e1.addAll(e);
+            return e1;
+            }
+            else{return null;}
+    }
 
-		return null;
-	}
+    public Ensemble<T> diff(Ensemble<? extends T> e) {
+         Ensemble<T> e1=new Ensemble<T>();
+         e1.addAll(table);
+         e1=inter(e);
+         this.removeAll(e1);
+         return this;
+    }
 
-	public Ensemble<T> inter(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
-
-		return null;
-	}
-
-	public Ensemble<T> diff(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
-
-		return null;
-	}
-
-	Ensemble<T> diffSym(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
-
-		return null;
-	}
-	
+    Ensemble<T> diffSym(Ensemble<? extends T> e) {
+    Ensemble<T> e1=new Ensemble<T>();
+    Ensemble<T> e2=new Ensemble<T>();
+         e1.addAll(table);
+         e1=inter(e);
+         e2=union(e);
+         e2.removeAll(e1);
+          return e2;
+    }
+    
 }
